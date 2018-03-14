b901b2cb6b591a807b533e509f65a4c21e315ce1
cdrake118 <cdrake@gmail.com>
2018-03-14 12:23:38 -0400

    Updates

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,24 +4,60 @@ import  SearchBar  from '../SearchBar/SearchBar';
 import  SearchResults  from '../SearchResults/SearchResults';
 import  Playlist  from '../Playlist/Playlist';
 
-/* sample data */
-let track = {
-  name: 'name',
-  artist: 'artist',
-  album: 'album'
-}
+
 
 class App extends Component {
     constructor(props){
         super(props)
         this.state = {
               searchResults: [{
-                name: 'name',
+                name: 'name2',
                 artist: 'artist',
-                album: 'album'
-              }]
+                album: 'album',
+              },
+              {
+                name: 'name23',
+                artist: 'artist',
+                album: 'album',
+              },
+              
+            ],
+
+            playlistName: '',
+
+            playlistTracks: [{
+              name: 'name3',
+              artist: 'artist',
+              album: 'album',
+            },
+            {
+              name: 'name4',
+              artist: 'artist',
+              album: 'album',
+            },
+          ]
         }
+        this.addTrack = this.addTrack.bind(this);
     }
+
+
+  removeTrack(track){
+    this.state.playlistTracks.filter(track.id)
+  }
+
+    addTrack(track){
+      let isNew = true;
+      for(let i=0; i<this.state.playlistTracks.length; i++){
+        if(this.track.id === this.state.playlistTracks.id){
+           isNew = false;
+           alert('This song is already part of your playlist.')
+        }
+      }
+      if(isNew){
+      this.playlistTracks.push(track);
+      }
+    }
+
     render() {
       return (
         <div>
@@ -29,8 +65,8 @@ class App extends Component {
         <div className="App">
           /* Add a SearchBar component */
           <div className="App-playlist">
-          <SearchResults searchResults={this.state.SearchResults}/>
-          /* Add a Playlist component */
+          <SearchResults searchResults={this.state.searchResults}/>
+          <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onAdd={this.addTrack}/>
           </div>
         </div>
       </div>
--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 import './Track.css';
 
 class Track extends Component {
+  constructor(props){
+    super(props)
+    this.addTrack = this.addTrack.bind(this);
+
+  }
+
+  addTrack(){
+    this.props.onAdd(this.props.track)
+  }
+
   renderAction(){
-    let isRemoval;
-    if(isRemoval){
-      return '+'
+    let isRemoval = true;
+    if(!isRemoval){
+      return <a className="Track-action" onClick={this.addTrack}> + </a>;
     }else{
-      return '-'
+      return <a className="Track-action" onClick={this.removeTrack}> - </a>;
     }
   }
   
@@ -18,7 +28,7 @@ class Track extends Component {
         <h3>{this.props.track.name}</h3>
         <p>{this.props.track.artist} | {this.props.track.album} </p>
       </div>
-      <a className="Track-action">{this.renderAction()}</a>
+      {this.renderAction()}
     </div>
     );
   }
